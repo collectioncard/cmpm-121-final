8bebd86722ef50c7e78ee6e6b44f7a4459149a6d
JackWilhelm <jgwilhel@ucsc.edu>
2024-11-18 12:31:15 -0800

    pulled from Art-Debug

--- farm-train/scripts/Global.cs
+++ farm-train/scripts/Global.cs
@@ -3,17 +3,17 @@ using System;
 
 public partial class Global : Node
 {
-    public const int TileWidth = 16;
-    public const int TileHeight = 16;
-    public static Vector2 SpriteOffset = new (8f, 8f);
-    public const int PlayerSpeed = 100;
-    public const float InteractionRadius = 4 * TileWidth;
-    public static Vector2I TileMapSize = new (40, 23);
+	public const int TileWidth = 16;
+	public const int TileHeight = 16;
+	public static Vector2 SpriteOffset = new (8f, 8f);
+	public const int PlayerSpeed = 100;
+	public const float InteractionRadius = 4 * TileWidth;
+	public static Vector2I TileMapSize = new (40, 23);
 
-    public static Vector2 GetTileAtPos(Vector2 globalPos)
-    {
-        double x = Math.Floor((globalPos.X) / TileWidth) * TileWidth;
-        double y = Math.Floor((globalPos.Y) / TileHeight) * TileHeight;
-        return new Vector2((float)x, (float)y);
-    }
+	public static Vector2 GetTileAtPos(Vector2 globalPos)
+	{
+		double x = Math.Floor((globalPos.X) / TileWidth) * TileWidth;
+		double y = Math.Floor((globalPos.Y) / TileHeight) * TileHeight;
+		return new Vector2((float)x, (float)y);
+	}
 }
