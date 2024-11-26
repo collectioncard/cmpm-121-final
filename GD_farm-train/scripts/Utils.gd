676088f713fd5beb5d67a206493054c493f63461
LifeHckr <jarodthereal@gmail.com>
2024-11-26 14:29:31 -0800

    Utils.gd
    Created

---
+++ GD_farm-train/scripts/Utils.gd
@@ -0,0 +1,21 @@
+class_name Utils #imagine a world where there are namespaces
+
+static func index_from_vec(tilePos : Vector2i) -> int:
+	if (
+			tilePos.x < 0
+			|| tilePos.y < 0
+			|| tilePos.x >= Global.TileMapSize.x
+			|| tilePos.y >= Global.TileMapSize.y
+		):
+			push_error("Invalid tilepos OOB");
+	return tilePos.x + (tilePos.y * Global.TILE_MAP_SIZE.x);
+
+static func vec_from_idx(idx: int) -> Vector2i:
+	if (idx < 0 || idx >= Global.TILE_MAP_SIZE.x * Global.TILE_MAP_SIZE.y):
+		push_error("Invald idx OOB");
+	return Vector2i(idx % Global.TILE_MAP_SIZE.x, idx / Global.TILE_MAP_SIZE.x);
+
+static func tile_from_vec(globalPos : Vector2i) -> Vector2i:
+		var x : int = floor((globalPos.x) / Global.TILE_WIDTH) * Global.TILE_WIDTH;
+		var y : int = floor((globalPos.y) / Global.TILE_HEIGHT) * Global.TILE_HEIGHT;
+		return Vector2i(x, y);
