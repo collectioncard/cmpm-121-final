aea2f06013eadad274c6ba735cd4fc7d6f57c6a6
Thomas Wessel <turpleturtle12@gmail.com>
2024-11-26 16:59:24 -0800

    Finish implementing TileDataManager
    It still needs tested, but basic stuff seems to work?

--- GD_farm-train/classes/TileDataManager.gd
+++ GD_farm-train/classes/TileDataManager.gd
@@ -1,10 +1,16 @@
 class_name TileDataManager extends Resource
 
-const DATA_SIZE = 4;
+# The size of our stored data in bytes.
+# We're using signed 32 bit integers for each property, so this should be 4 bytes. (can't do sizeOf in GDscript)
+const DATA_SIZE := 4;
 
+# The size each tile takes up in the tile_data array. Used to calculate offsets n stuff.
+# As of now, we have 6 properties, so this should be 6 * DATA_SIZE = 24 bytes per tile.
+# In other words, each tile starts at multiples of 24 bytes in the tile_data array. (0, 24, 48, 72, etc)
+var tile_info_size: int = properties.size() * DATA_SIZE;
 
-# This isnt compile time constant because GDscript doesnt support that. Please dont add anything to this enum
-# during runtime.
+# This isnt compile time constant because GDscript doesnt support that. Please *DO NOT* add anything to this enum during runtime.
+# Cant enforce that though. :(
 enum properties {
 	SOIL_TYPE = 1 * DATA_SIZE,
 	PLANT_ID = 2 * DATA_SIZE,
@@ -14,40 +20,67 @@ enum properties {
 	COORD_Y = 6 * DATA_SIZE
 }
 
-var tile_info_size: int = properties.size() * DATA_SIZE;
-
-
-
 # The GDscript packedByteArray that holds the tile data
-
-@export var tile_data : PackedByteArray = PackedByteArray();
-
+var tile_data : PackedByteArray = PackedByteArray();
 
 func _init():
 	tile_data.resize(Global.TILE_MAP_SIZE.x * Global.TILE_MAP_SIZE.y * tile_info_size);
 
 
+####**** Get Tile Data ****####
+
+
+#Gets the value of a specific property at a given coordinate.
+#params:
+#property: The property to retrieve the value for.
+#tilePos: The position of the tile.
+#returns:The value of the specified property at the given coordinate.
 func get_property_value_at_coord(property: properties, tilePos: Vector2i) -> int:
 	return tile_data.decode_s32(Utils.index_from_vec(tilePos) * tile_info_size + property);
 
+#Gets the tile info at a given coordinate.
+#params:
+#tilePos: The position of the tile in the world.
+func get_tile_info_at_coord(tilePos: Vector2i) -> TileInfo:
+	var tileInfo = TileInfo.new(
+		get_property_value_at_coord(properties.SOIL_TYPE, tilePos),
+		get_property_value_at_coord(properties.PLANT_ID, tilePos),
+		get_property_value_at_coord(properties.MOISTURE_LEVEL, tilePos),
+		get_property_value_at_coord(properties.GROWTH_STAGE, tilePos),
+		get_property_value_at_coord(properties.COORD_X, tilePos),
+		get_property_value_at_coord(properties.COORD_Y, tilePos)
+	);
 
+	return tileInfo;
 
-func get_tile_info_at_coord(tilePos) -> TileInfo:
-	##TODO: Implement this function
-	return null;
+####**** Set Tile Data ****####
 
-func set_property_value_at_coord(property, tilePos, value):
+#Sets the value of a specific property at a given coordinate.
+#params:
+#property: The property to set the value for.
+#tilePos: The position of the tile in the world.
+#value: The int value to set the property to.
+func set_property_value_at_coord(property: properties, tilePos: Vector2i, value: int):
 	tile_data.encode_s32(Utils.index_from_vec(tilePos) * tile_info_size + property, value);
 
 
-func set_tile_info_at_coord(tilePos, tileInfo):
-	#TODO: Implement this function
-	pass;
+#Sets the tile info at a given coordinate.
+#params:
+#tilePos: The position of the tile in the world.
+#tileInfo: The TileInfo object to set the tile to.
+func set_tile_info_at_coord(tilePos: Vector2i, tileInfo: TileInfo):
+	var tile_index := Utils.tile_from_vec(tilePos);
+	set_property_value_at_coord(properties.SOIL_TYPE, tile_index, tileInfo.soil_type);
+	set_property_value_at_coord(properties.PLANT_ID, tile_index, tileInfo.plant_id);
+	set_property_value_at_coord(properties.MOISTURE_LEVEL, tile_index, tileInfo.moisture_level);
+	set_property_value_at_coord(properties.GROWTH_STAGE, tile_index, tileInfo.growth_stage);
+	set_property_value_at_coord(properties.COORD_X, tile_index, tileInfo.coord_x);
+	set_property_value_at_coord(properties.COORD_Y, tile_index, tileInfo.coord_y);
 
+####**** Export and Clear Tile Data ****####
 
 func export_tile_data() -> PackedByteArray:
-	#TODO: Implement this function
-	return PackedByteArray();
+	return tile_data.duplicate();
 
 func clear_tile_data():
 	tile_data.clear();
@@ -58,8 +91,6 @@ func overwrite_tile_data(data: PackedByteArray):
 		return;
 
 	tile_data = data.duplicate();
-
-
 
 class TileInfo:
 	var soil_type := 0;
--- GD_farm-train/scripts/Utils.gd
+++ GD_farm-train/scripts/Utils.gd
@@ -1,6 +1,6 @@
 extends Node
 
-static func index_from_vec(tilePos : Vector2i) -> int:
+func index_from_vec(tilePos : Vector2i) -> int:
 	if (
 			tilePos.x < 0
 			|| tilePos.y < 0
@@ -10,12 +10,12 @@ static func index_from_vec(tilePos : Vector2i) -> int:
 			push_error("Invalid tilepos OOB");
 	return tilePos.x + (tilePos.y * Global.TILE_MAP_SIZE.x);
 
-static func vec_from_idx(idx: int) -> Vector2i:
+func vec_from_idx(idx: int) -> Vector2i:
 	if (idx < 0 || idx >= Global.TILE_MAP_SIZE.x * Global.TILE_MAP_SIZE.y):
 		push_error("Invald idx OOB");
 	return Vector2i(idx % Global.TILE_MAP_SIZE.x, idx / Global.TILE_MAP_SIZE.x);
 
-static func tile_from_vec(globalPos : Vector2i) -> Vector2i:
+func tile_from_vec(globalPos : Vector2i) -> Vector2i:
 		var x : int = floor((globalPos.x) / Global.TILE_WIDTH) * Global.TILE_WIDTH;
 		var y : int = floor((globalPos.y) / Global.TILE_HEIGHT) * Global.TILE_HEIGHT;
 		return Vector2i(x, y);
